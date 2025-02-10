2fd1d9d5fef409920cc444fbd5496b500a8ab280
unnati kute <unnatikute777@gmail.com>
2025-02-10 21:03:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('H')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
